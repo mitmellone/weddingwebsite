5f37a3c33267c409e987fa1d3e7d9998609cb110
Mitchell Mellone <mitchell.mellone@gmail.com>
2022-06-18 13:00:18 -0500

    Changed to typescript

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media-api.xogrp.com/images/030c46db-0fbb-49fb-86b1-b23ad2792a95~rt_auto-cr_0.0.5083.3389-rs_5083.3389?ordering=explicit" className="App-logo" alt="logo" />
+        <p>
+          Welcome to Kel and Mitchell's seating chart.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.theknot.com/kelandmitchell"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to the knot
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
