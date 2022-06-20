ff00297422d2ceb9b8ad86fd9d27e2c2ee5cb4b4
Mitchell Mellone <mitchell.mellone@gmail.com>
2022-06-19 22:42:24 -0500

    Add embedded youtube video player

--- src/components/FindYourSeat/FindYourSeat.tsx
+++ src/components/FindYourSeat/FindYourSeat.tsx
@@ -1,8 +1,38 @@
-import { Autocomplete, TextField, Typography } from "@mui/material";
+import { Autocomplete, TextField, Typography, styled } from "@mui/material";
 import { useState } from "react";
 
 // todo fetch from api
 const names = ["Steve Mellone", "Lisa Mellone", "Neal Mathes", "Mary Farahmand"];
+const data = [
+  {
+    name: "Steve Mellone",
+    table: 1,
+    artist: "The Black Keys",
+    videoEmbedId: "6yCIDkFI7ew",
+  },
+  {
+    name: "Lisa Mellone",
+    table: 1,
+    artist: "The Black Keys",
+    videoEmbedId: "6yCIDkFI7ew",
+  },
+  {
+    name: "Neal Mathes",
+    table: 1,
+    artist: "Ripe",
+    videoEmbedId: "gmE-33ZGOfg",
+  },
+  {
+    name: "Mary Farahmand",
+    table: 1,
+    artist: "FINNEAS",
+    videoEmbedId: "VaKzNtwPQxE",
+  },
+];
+
+function getDataForName(nameQuery: string) {
+  return data.find(({ name }) => name === nameQuery);
+}
 
 export default function FindYourSeat() {
   const [selectedName, setSelectedName] = useState<string | null>(null);
@@ -20,9 +50,50 @@ export default function FindYourSeat() {
         onInputChange={(_, newInputValue) => setInputValue(newInputValue)}
         options={names}
         fullWidth
-        open
         renderInput={(params) => <TextField {...params} label="Select your name" />}
       />
+      {selectedName && <YoutubeVideo embedId={getDataForName(selectedName)?.videoEmbedId} />}
     </>
+  );
+}
+
+interface YoutubeVideoProps {
+  embedId?: string;
+}
+
+const VideoResponsiveContainer = styled("div")(({ theme }) => ({
+  overflow: "hidden",
+  paddingBottom: theme.spacing(2),
+  position: "relative",
+  display: "flex",
+  height: 360,
+  marginTop: theme.spacing(2),
+  "& iframe": {
+    left: 0,
+    top: 0,
+    paddingTop: 120,
+    border: "none",
+    marginTop: -60,
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+  },
+}));
+
+function YoutubeVideo({ embedId }: YoutubeVideoProps) {
+  if (!embedId) {
+    return <Typography variant="body1">Who are you?</Typography>;
+  }
+  return (
+    <VideoResponsiveContainer>
+      <Typography variant="h5">Can you name the artist?</Typography>
+      <iframe
+        width="360"
+        height="360"
+        src={`https://www.youtube.com/embed/${embedId}?modestbranding=1&autoplay=1`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        title="Do you know the artist?"
+      />
+    </VideoResponsiveContainer>
   );
 }
--- src/components/common/PageLayout.tsx
+++ src/components/common/PageLayout.tsx
@@ -1,12 +1,11 @@
 import { Container, styled } from "@mui/material";
 import { PropsWithChildren } from "react";
 
-export default function PageLayout({ children }: PropsWithChildren<{}>) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const StyledContainer = styled(Container)(({ theme }) => ({
-    textAlign: "center",
-    margin: theme.spacing(4, 0),
-  }));
+const StyledContainer = styled(Container)(({ theme }) => ({
+  textAlign: "center",
+  margin: theme.spacing(4, 0),
+}));
 
+export default function PageLayout({ children }: PropsWithChildren<{}>) {
   return <StyledContainer maxWidth="sm">{children}</StyledContainer>;
 }
